ca750d32dc00af2ddd56559090e2243e8e77f914
ABS <aashishbibyan007@gmail.com>
2023-04-10 00:58:11 +0530

    Application done

--- lib/pages/text_checker.dart
+++ lib/pages/text_checker.dart
@@ -15,28 +15,24 @@ class _TextCheckerState extends State<TextChecker> {
   final TextEditingController _controller = TextEditingController();
 
   Future<void> detectText() async {
-    final url = Uri.parse(
-        "https://neutrinoapi-bad-word-filter.p.rapidapi.com/bad-word-filter");
-    final headers = {
+    var uri = Uri.parse(
+        "https://profanity-cleaner-bad-word-filter.p.rapidapi.com/profanity");
+    var headers = {
       'content-type': 'application/json',
-      'X-RapidAPI-Key': '6c0c776a66msh536572f5dfb2d98p1d0b06jsn8b89eeb288c1',
-      'X-RapidAPI-Host': 'neutrinoapi-bad-word-filter.p.rapidapi.com'
+      'X-RapidAPI-Key': '6c0c776a66msh536572f5dfb2d98p1d0b06jsn8b89eeb288c1'
     };
-    final body = json.encode({"content": _controller.text});
-    final response = await http.post(url, headers: headers, body: body);
-    debugPrint(response.body);
+    var body = json.encode({
+      "text": _controller.text,
+      "maskCharacter": "x",
+      "language": "en"
+    });
+    var response = await http.post(uri, headers: headers, body: body);
     if (response.statusCode == 200) {
-      final jsonresponse = jsonDecode(response.body);
-      final bool iscussed = jsonresponse["is-bad"];
-      if (iscussed == true) {
-        ScaffoldMessenger.of(context).showSnackBar(
-            SnackBar(content: Text("This text contains cuss word")));
-      } else {
-        ScaffoldMessenger.of(context)
-            .showSnackBar(SnackBar(content: Text("Approved")));
-      }
+      debugPrint(response.body.toString());
+      ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(content: Text("This text contains cuss words.")));
     } else {
-      debugPrint("Request failed with status: ${response.statusCode}");
+      debugPrint("Error Code ${response.statusCode}");
     }
   }
 
